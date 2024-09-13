014d6469f8a7a01923568a2bf273857b66dc2250
Durojaiye Isaiah <isaiahdurojaiye9@gmail.com>
2024-09-13 14:25:43 +0100

    changes

--- homepage.js
+++ homepage.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
         allDay: true
       },
       {
-        title: 'Independence Day',
+        title: 'Independence Day 💚🤍💚',
         start: '2024-10-01',
         allDay: true
       },
@@ -50,27 +50,27 @@ document.addEventListener('DOMContentLoaded', function () {
         allDay: true
       },
       {
-        title: 'Monthly HSE Meeting',
+        title: 'Monthly HSE Meeting 📃',
         start: '2024-09-30T13:00:00',
         end: '2024-09-30T14:00:00',
       },
       {
-        title: 'Monthly HSE Meeting',
+        title: 'Monthly HSE Meeting 📃',
         start: '2024-10-28T13:00:00',
         end: '2024-10-28T14:00:00',
       },
       {
-        title: 'Monthly HSE Meeting',
+        title: 'Monthly HSE Meeting 📃',
         start: '2024-11-25T13:00:00',
         end: '2024-11-25T14:00:00',
       },
       {
-        title: 'End of the Year Gift Distribution',
+        title: 'End of the Year Gift Distribution 🎁',
         start: '2024-12-11',
         end: '2024-12-14',
       },
       {
-        title: 'Monthly HSE Meeting',
+        title: 'Monthly HSE Meeting 📃',
         start: '2024-12-16T13:00:00',
         end: '2024-12-16T14:00:00',
       },
@@ -80,14 +80,179 @@ document.addEventListener('DOMContentLoaded', function () {
         end: '2024-12-21',
       },
       {
-        title: 'Christmas Holiday',
+        title: 'Christmas Holiday 🎄🎅🏽',
         start: '2024-12-23',
         end: '2024-12-27',
       },
       {
-        title: "New Year's Holiday",
+        title: "New Year's Holiday 🎆✨",
         start: '2024-12-31',
         end: '2025-01-02',
+      },
+      {
+        title: "Valentine's Day 💝💘",
+        start: '2025-02-14',
+        allDay: true
+      },
+      {
+        title: "Ash Wednesday",
+        start: '2025-03-05',
+        allDay: true
+      },
+      {
+        title: "Women's Day",
+        start: '2025-03-08',
+        allDay: true
+      },
+      {
+        title: "Good Friday",
+        start: '2025-04-18',
+        allDay: true
+      },
+      {
+        title: "Holy Saturday",
+        start: '2025-04-19',
+        allDay: true
+      },
+      {
+        title: "Easter Sunday",
+        start: '2025-04-20',
+        allDay: true
+      },
+      {
+        title: "Easter Monday",
+        start: '2025-04-21',
+        allDay: true
+      },
+      {
+        title: "Workers' Day 👨🏽‍🏭👮🏽‍♂️",
+        start: '2025-05-01',
+        allDay: true
+      },
+      {
+        title: "Children's Day 👩‍👦",
+        start: '2025-05-27',
+        allDay: true
+      },
+      {
+        title: "Father's Day 👔",
+        start: '2025-06-15',
+        allDay: true
+      },
+      {
+        title: "Mr Adetola's Birthday 🎈🎉",
+        start: '2025-01-20',
+        allDay: true
+      },
+      {
+        title: "Mr Timothy's Birthday 🎈🎉",
+        start: '2025-02-03',
+        allDay: true
+      },
+      {
+        title: "Mr Dapo's Birthday 🎈🎉",
+        start: '2025-06-13',
+        allDay: true
+      },
+      {
+        title: "Mr Ladi's Birthday 🎈🎉",
+        start: '2025-04-17',
+        allDay: true
+      },
+      {
+        title: "Mr Chidi's Birthday 🎈🎉",
+        start: '2024-10-31',
+        allDay: true
+      },
+      {
+        title: "Mr Chidi's Birthday 🎈🎉",
+        start: '2025-10-31',
+        allDay: true
+      },
+      {
+        title: "Mr Gbemis's Birthday 🎈🎉",
+        start: '2025-03-13',
+        allDay: true
+      },
+      {
+        title: "Miss Sandra's Birthday 🎈🎉",
+        start: '2024-12-29',
+        allDay: true
+      },
+      {
+        title: "Miss Sandra's Birthday 🎈🎉",
+        start: '2025-12-29',
+        allDay: true
+      },
+      {
+        title: "Mrs Onyinyechi's Birthday 🎈🎉",
+        start: '2024-11-11',
+        allDay: true
+      },
+      {
+        title: "Mrs Onyinyechi's Birthday 🎈🎉",
+        start: '2025-11-11',
+        allDay: true
+      },
+      {
+        title: "Miss Abosede's Birthday 🎈🎉",
+        start: '2025-08-20',
+        allDay: true
+      },
+      {
+        title: "Miss Cecelia's Birthday 🎈🎉",
+        start: '2025-05-06',
+        allDay: true
+      },
+      {
+        title: "Mr Benjamin's Birthday 🎈🎉",
+        start: '2025-05-27',
+        allDay: true
+      },
+      {
+        title: "Mr Joel's Birthday 🎈🎉",
+        start: '2025-01-21',
+        allDay: true
+      },
+      {
+        title: "Mr Henry's Birthday 🎈🎉",
+        start: '2025-09-20',
+        allDay: true
+      },
+      {
+        title: "Democracy Day",
+        start: '2025-06-12',
+        allDay: true
+      },
+      {
+        title: "Mr Henry's Birthday 🎈🎉",
+        start: '2024-09-20',
+        allDay: true
+      },
+      {
+        title: "Mr Itoro's Birthday 🎈🎉",
+        start: '2024-11-18',
+        allDay: true
+      },
+      {
+        title: "Mr Itoro's Birthday 🎈🎉",
+        start: '2025-11-18',
+        allDay: true
+      },
+      {
+        title: "Mr Bamishaye's Birthday 🎈🎉",
+        start: '2025-06-12',
+        allDay: true
+      },
+      {
+        title: "Mr Kevin's Birthday 🎈🎉",
+        start: '2025-05-05',
+        allDay: true
+      },
+      {
+        title: "Mr Uche's Birthday 🎈🎉",
+        start: '2025-04-02',
+        allDay: true
       }
     ]
   });
